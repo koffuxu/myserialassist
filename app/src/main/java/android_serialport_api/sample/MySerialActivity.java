3db27bf81efec098b2ff4c011ff0ca6b1cf8eea3
koffuxu <koffuxu@gmial.com>
2017-06-08 00:22:46 +0800

    MySerial: add Receive Data As Hex

--- app/src/main/java/android_serialport_api/sample/MySerialActivity.java
+++ app/src/main/java/android_serialport_api/sample/MySerialActivity.java
@@ -19,6 +19,8 @@ public class MySerialActivity extends SerialPortActivity {
     private EditText mSend;
     private EditText mReception;
     private RadioGroup mRadioGrop;
+    private boolean isSendAsHex = false;
+    private boolean isRecvAsHex = false;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +38,14 @@ public class MySerialActivity extends SerialPortActivity {
             public void onClick(View v) {
                 int i;
                 CharSequence t = mSend.getText();
-                Log.i(TAG, "send Text:"+mSend.getText());
+                Log.i(TAG, "send Text as Assic:"+mSend.getText());
                 char[] text = new char[t.length()];
                 for (i=0; i<t.length(); i++) {
                     text[i] = t.charAt(i);
                 }
                 try {
                     mOutputStream.write(new String(text).getBytes());
-                    mOutputStream.write('\n');
+                   // mOutputStream.write('\n');
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -61,6 +63,7 @@ public class MySerialActivity extends SerialPortActivity {
                 len = t.length();
                 byte[] b = new byte[len/2];
                 //check the string is Hex String
+                Log.d(TAG, "send Tex as Hex"+mSend.getText());
                 if(len % 2 != 0){
                     Log.i(TAG, "The counts of String is not EVEN!");
                     Toast.makeText(getApplicationContext(), "Error: hex not EVEN counts!",
@@ -83,8 +86,8 @@ public class MySerialActivity extends SerialPortActivity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    Toast.makeText(getApplicationContext(), "send as hex",
-                            Toast.LENGTH_SHORT).show();
+                   // Toast.makeText(getApplicationContext(), "send as hex",
+                   //         Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(getApplicationContext(), "Error when send as hex",
                             Toast.LENGTH_SHORT).show();
@@ -109,9 +112,37 @@ public class MySerialActivity extends SerialPortActivity {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if(checkedId == R.id.rBAssic){
+                    isRecvAsHex = false;
                     Log.d(TAG, "Receive show as Assic");
+                    if(mReception != null) {
+                        //conver 16 string to assic = char string
+                        //1,get the CharSquence
+                        CharSequence cs = mReception.getText();
+                        //2, Byte Array
+                        byte[] bytes = HexDump.hexStringToByteArray(cs.toString());
+                        Log.d(TAG, "==Conver== Hex:"+cs.toString()+" ==to Bytes== "+bytes);
+                        for(int i =0 ;i<bytes.length; i++) {
+                            Log.d(TAG,"before:"+ bytes[i]+" after (char): "+(char)bytes[i]);
+                            mReception.setText(bytes[i]);
+                        }
+                    }
+
                 } else if (checkedId == R.id.rBHex) {
+                    isRecvAsHex = true;
                     Log.d(TAG, "Receive show as Hex");
+                    //covert string to hex
+                    if(mReception != null){
+                       CharSequence cs = mReception.getText();
+                        int charlen = cs.length();
+                        for(int i = 0; i < charlen; i++){
+                            //char to assic. the charValue(shijinzhi) can search in assic table
+                            int charValue = (int)cs.charAt(i);
+                            String toShow = HexDump.toHexString((byte) charValue);
+                            Log.d(TAG, "==Convert== Char: "+ charValue +" ==to HEX==> "+toShow);
+                            mReception.setText(toShow);
+                        }
+
+                    }
                 } else {
                     Log.d(TAG, "Error");
                 }
@@ -122,11 +153,16 @@ public class MySerialActivity extends SerialPortActivity {
 
 	@Override
     protected void onDataReceived(final byte[] buffer, final int size) {
-        Log.d(TAG, "Received " + size +"byte Data:" + new String(buffer,0,size));
+        Log.d(TAG, "Received " + size +" byte Data:" + new String(buffer,0,size));
 		runOnUiThread(new Runnable() {
 			public void run() {
 				if (mReception != null) {
-					mReception.append(new String(buffer, 0, size));
+                    if(isRecvAsHex == true){
+                        Log.d(TAG, "Receiver show in Hex");
+                        mReception.append(HexDump.toHexString(buffer, 0, size));
+                    } else {
+                        mReception.append(new String(buffer, 0, size));
+                    }
 				}
 			}
 		});
